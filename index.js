911c46f04ac09f4076742b5b510b4899679578cc
DevHeris <badairoinioluwa578@gmail.com>
2024-01-10 20:50:57 +0200

    Generated the URL

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const input = document.querySelector("#message-input");
-  const encrypted = btoa(input.value);
+  const messageInput = document.querySelector("#message-input");
+  const encrypted = btoa(messageInput.value);
 
-  document.querySelector("#link-input").value = encrypted;
+  const linkInput = document.querySelector("#link-input");
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
 });
